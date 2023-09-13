901086a3fd675035c76b957cbc594914e1953627
MASTERGOKUL <selvarajgokul09@gmail.com>
2023-09-13 22:50:26 +0530

    maked the link for daily from full

--- src/components/fulltimetable.js
+++ src/components/fulltimetable.js
@@ -1,12 +1,18 @@
 import { SmallBox } from "./smallBox";
 import "./main.css";
 function Full() {
-    return (
-        <div style={{display:"flex",flexDirection:"column"}}>
-           <iframe title="sheet" style={{height:"85vh",width:"100%"}} src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQTR2vDR7_whn6sv9VGVDZmEKaiHUgNpX1OtPIv7-KOq6HGU6uDvVfTzF5cIrgcmA/pubhtml"/>
-            <SmallBox color="#FFD6EF" text="Week" flag="true" route="/week"/>
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <iframe
+        title="sheet"
+        style={{ height: "90vh", width: "100%" }}
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQTR2vDR7_whn6sv9VGVDZmEKaiHUgNpX1OtPIv7-KOq6HGU6uDvVfTzF5cIrgcmA/pubhtml"
+      />
+      <div id="full-route">
+        <SmallBox color="#FFD6EF" text="Week" flag="true" route="/week" />
+        <SmallBox color="#FFD6EF" text="Daily" flag="true" route="/" />
+      </div>
+    </div>
+  );
 }
 export default Full;
-    
