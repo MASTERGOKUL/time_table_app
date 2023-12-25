a2a2cf370a1e5494e4e5795ad139bac702c77741
MASTERGOKUL <selvarajgokul09@gmail.com>
2023-12-25 09:21:08 +0530

    fixed the bug in bottom_nav

--- src/components/bottomnav/bottom_nav.js
+++ src/components/bottomnav/bottom_nav.js
@@ -1,67 +1,52 @@
-import React, { useState } from "react";
-import { Link, useNavigate  } from "react-router-dom";
+import React from "react";
+import { Link , useLocation } from "react-router-dom";
 import "./bottom_nav.css";
-import { useEffect } from "react";
 
 
 export function BottomNav() {
-  // Using state to manage the active item
-  const [activeItem, setActiveItem] = useState(
-    parseInt(localStorage.getItem("activeItem")) || 0
-  );
 
-  // Access the history object to navigate programmatically
-  const history = useNavigate ();
+  const location = useLocation();
+  console.log(location);
+  const location_path = location.pathname;
 
-  // Function to handle click events and update the active item
-  const handleItemClick = (index, path) => {
-    setActiveItem(index);
-    history.push(path); // Navigate to the specified path
-  };
-
-    // Update local storage when the active item changes
-    useEffect(() => {
-      localStorage.setItem("activeItem", activeItem);
-    }, [activeItem]);
-  
 
   return (
     <div className="navigation">
       <ul>
-        <li className={`list ${activeItem === 0 ? 'active' : ''}`}>
-          <Link to="/" onClick={() => handleItemClick(0, "/")}>
+        <li className={`list ${location_path === '/' ? 'active' : ''}`}>
+          <Link to="/">
             <span className="icon">
               <ion-icon name="home-outline"></ion-icon>
             </span>
             <span className="text">Today</span>
           </Link>
         </li>
-        <li className={`list ${activeItem === 1 ? 'active' : ''}`}>
-          <Link to="/week" onClick={() => handleItemClick(1, "/week")}>
+        <li className={`list ${location_path === '/week' ? 'active' : ''}`}>
+          <Link to="/week">
             <span className="icon">
               <ion-icon name="calendar-outline"></ion-icon>
             </span>
             <span className="text">Week</span>
           </Link>
         </li>
-        <li className={`list ${activeItem === 2 ? 'active' : ''}`}>
-          <Link to="/full" onClick={() => handleItemClick(2,'/full')}>
+        <li className={`list ${location_path === '/full' ? 'active' : ''}`}>
+          <Link to="/full">
             <span className="icon">
               <ion-icon name="cube-outline"></ion-icon>
             </span>
             <span className="text">Full</span>
           </Link>
         </li>
-        <li className={`list ${activeItem === 3 ? 'active' : ''}`}>
-          <Link to="/exam" onClick={() => handleItemClick(3,'/exam')}>
+        <li className={`list ${location_path === '/exam' ? 'active' : ''}`}>
+          <Link to="/exam">
             <span className="icon">
               <ion-icon name="pencil-outline"></ion-icon>
             </span>
             <span className="text">Exam</span>
           </Link>
         </li>
-        <li className={`list ${activeItem === 4 ? 'active' : ''}`}>
-          <Link to="/info" onClick={() => handleItemClick(4,'/info')}>
+        <li className={`list ${location_path === '/info' ? 'active' : ''}`}>
+          <Link to="/info">
             <span className="icon">
               <ion-icon name="information-circle-outline"></ion-icon>
             </span>
--- src/components/full/fulltimetable.js
+++ src/components/full/fulltimetable.js
@@ -4,7 +4,7 @@ function Full() {
     <div style={{ display: "flex", flexDirection: "column" }}>
       <iframe
         title="sheet"
-        style={{ height: "85dvh", width: "100%" }}
+        style={{ height: "85dvh", width: "90wdh" }}
         // src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQTR2vDR7_whn6sv9VGVDZmEKaiHUgNpX1OtPIv7-KOq6HGU6uDvVfTzF5cIrgcmA/pubhtml"
         // src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROmZZaOnEERg1OPPCpqN9ntMmIDGsxF6ixYPtHsCAckb_I7e1iQ9ZyvX4b9e4Eyw/pubhtml"
         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTsoP0O1sANntTLmKwM-VSMYovACBxuzt_0GAaoRfPB-uq09vcRiNvVTYDL1wZO5g/pubhtml"
