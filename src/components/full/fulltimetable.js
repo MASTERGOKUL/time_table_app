317fd36098ee0eae8e0b3d6bf69726acf332df22
GOKUL S <94166506+MASTERGOKUL@users.noreply.github.com>
2024-05-14 20:40:02 +0530

    Update fulltimetable.js

--- src/components/full/fulltimetable.js
+++ src/components/full/fulltimetable.js
@@ -7,8 +7,9 @@ function Full() {
         style={{ height: "85dvh", width: "90wdh" }}
         // src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQTR2vDR7_whn6sv9VGVDZmEKaiHUgNpX1OtPIv7-KOq6HGU6uDvVfTzF5cIrgcmA/pubhtml"
         // src="https://docs.google.com/spreadsheets/d/e/2PACX-1vROmZZaOnEERg1OPPCpqN9ntMmIDGsxF6ixYPtHsCAckb_I7e1iQ9ZyvX4b9e4Eyw/pubhtml"
-        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTsoP0O1sANntTLmKwM-VSMYovACBxuzt_0GAaoRfPB-uq09vcRiNvVTYDL1wZO5g/pubhtml"
-      />
+        //src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTsoP0O1sANntTLmKwM-VSMYovACBxuzt_0GAaoRfPB-uq09vcRiNvVTYDL1wZO5g/pubhtml"
+      src = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTa0mnC5B1fkkUPpYFDf6z3NCin07H_Ahp7ktmGz6gyhdTMIHvV6ZvZJrQIENsseA/pubhtml"
+/>
     </div>
   );
 }
